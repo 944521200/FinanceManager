e44fa7822cc9cbef851cfa0d443f9b5bc19e91a7
Alfonso HH <alfonxd@gmail.com>
2021-10-10 21:41:39 +0200

    basic components, routing and some placeholders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ManageComponent } from './manage/manage.component';
+import { ReportComponent } from './report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'manage', component: ManageComponent },
+  { path: 'report', component: ReportComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReportComponent } from './report/report.component';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReportComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
