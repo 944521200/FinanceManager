b02881791e67617d8f7948ac5eaaab038df28235
Alfonso HH <alfonxd@gmail.com>
2021-10-24 19:16:36 +0200

    nuevos graficos

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -9,67 +9,20 @@ Chart.register(...registerables)
 })
 export class ReportComponent implements OnInit {
   constructor() {}
-
+/**
+ * 
+ *  un gráfico que muestre el menor y mayor gasto mensual de cada tag o de cada expense
+ * 
+ */
   ngOnInit(): void {
 
-    const config = {
+    var expensesChart = new Chart(document.getElementById('expenesByMonth') as ChartItem,{
       type: 'line',
       data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June'],
         datasets: [
           {
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-          },
-        ],
-      },
-      options: {},
-    };
-    const config2= {
-      type: 'bar',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
-  }
-
-
-    var myChart = new Chart(document.getElementById('myChart') as ChartItem,{
-      type: 'line',
-      data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [
-          {
-            label: 'My First dataset',
+            label: 'Expenses by month',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45],
@@ -78,5 +31,68 @@ export class ReportComponent implements OnInit {
       },
       options: {},
     });
+
+    var expensesByTag =  new Chart(document.getElementById('expensesByTag') as ChartItem,{
+      type: 'doughnut',
+      data: {
+        labels: [
+          'anime',
+          'food',
+          'games'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+    });
+    
+    var topExpenses = new Chart(document.getElementById('topExpenses') as ChartItem,{
+      type: 'bar',
+      data: {
+        labels: ['Audi a4', 'Gaming gamer chair','dick enhancement', 'konosuba bluray', 'HDD','webcam','boli bic'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [20000, 800, 400, 180, 56, 55, 2],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    });
+
+
+
   }
+
+
 }
