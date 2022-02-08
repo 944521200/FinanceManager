e34e598adc5e819216c1e88667f001a5874e9e93
Alfonso HH <alfonxd@gmail.com>
2022-02-08 19:41:15 +0100

    Prettier format

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.alfonsohh.financemanager',
-  appName: 'financeManager',
-  webDir: 'dist/financeManager',
-  bundledWebRuntime: false
+    appId: 'com.alfonsohh.financemanager',
+    appName: 'financeManager',
+    webDir: 'dist/financeManager',
+    bundledWebRuntime: false,
 };
 
 export default config;
